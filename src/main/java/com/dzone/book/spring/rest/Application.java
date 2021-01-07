b1129846309bce4ab40a5e2fea2d6486f7a40f34
Justin Albano <albano.justin@gmail.com>
2021-01-07 18:07:58 -0500

    Used standalone configuration

--- src/main/java/com/dzone/book/spring/rest/Application.java
+++ src/main/java/com/dzone/book/spring/rest/Application.java
@@ -2,16 +2,12 @@ package com.dzone.book.spring.rest;
 
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
         
-        try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(Application.class)) {
+        try (ConfigurableApplicationContext context = new AnnotationConfigApplicationContext(Config.class)) {
             Car car = context.getBean(Car.class);
             car.start();
         }
---
+++ src/main/java/com/dzone/book/spring/rest/Config.java
@@ -0,0 +1,8 @@
+package com.dzone.book.spring.rest;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.dzone.book.spring.rest")
+public class Config {}
